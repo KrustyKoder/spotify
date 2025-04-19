7cf3bf306be3da9e681ab280f6b36a9f6eec3a96
KrustyKoder <sarit.stanley.stanyer@gmail.com>
2025-04-19 18:45:54 +0100

    hey

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Playlist from './Playlist';
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
+import Track from './Track';
+import TrackList from './Tracklist';
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar></SearchBar>
     </div>
   );
+
+
+
+
 }
 
 export default App;
